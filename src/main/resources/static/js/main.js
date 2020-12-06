91fac49d051772fb231bbd2faedba391945ff128
LuisSe25 <luissesarego@outlook.com>
2020-12-06 14:59:12 -0500

    fix admin 1

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
 			cancelButtonText: 'No, cancelar'
 		}).then((result) => {
 			if (result.value) {
-				window.location="http://localhost:8090/login";
+				window.location="login";
 			}
 		});
 	});
